c4e2f92eca5d2e7308c5529db2307ad8bbd8270b
PrincePatel027 <princeghadiya699@gmail.com>
2024-12-12 15:50:56 +0530

    Fix search functionality.

--- lib/controller/home_controller.dart
+++ lib/controller/home_controller.dart
@@ -4,7 +4,7 @@ import 'package:linktree_prinx/helper/api_helper.dart';
 class HomeController extends GetxController {
   var searchText = ''.obs;
   var isLoading = false.obs;
-  var users = <dynamic>[].obs;
+  var users = [].obs;
   var errorMessage = ''.obs;
 
   @override
@@ -29,7 +29,12 @@ class HomeController extends GetxController {
     try {
       isLoading.value = true;
       errorMessage.value = '';
-      users.value = await ApiHelper.apiHelper.fetchSingleUser(name: name);
+      List? data = await ApiHelper.apiHelper.fetchSingleUser(name: name);
+      if (data == null) {
+        users = [].obs;
+      } else {
+        users.value = data;
+      }
     } catch (e) {
       errorMessage.value = e.toString();
     } finally {
